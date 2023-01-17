710bb5738ef0108caec2f2dfdb1903a6d69f8477
frduzn <117011377+frduzn@users.noreply.github.com>
2023-01-17 23:27:39 +0300

    first commit

---
+++ client/client/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;//
+using System.Net;//
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Socket client = null;
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipaddr = null;
+            try
+            {
+                Console.WriteLine("*** TCP/IP Soket Uygulaması -- Ferdi ÜZEN");
+                Console.WriteLine("Ip Adresini Giriniz ve Enter Tuşuna Basınız -- Varsayılan: 127.0.0.1");
+
+                string strIPAddress = Console.ReadLine();
+
+                Console.WriteLine(" Port Numarasını Giriniz -- Varsayılan:34000");
+                string strPortInput = Console.ReadLine();
+
+
+                int nPortInput = 0;
+
+                if (!IPAddress.TryParse(strIPAddress, out ipaddr))
+                {
+                    Console.WriteLine("Geçersiz sunucu adresi girişi yapıldı!");
+                    return;
+                }
+                if (!int.TryParse(strPortInput.Trim(), out nPortInput))
+                {
+                    Console.WriteLine("Geçersiz port adresi girişi yapıldı!");
+                    return;
+                }
+
+                if (nPortInput <= 0 || nPortInput > 65535)
+                {
+                    Console.WriteLine("Port numarası 0 and 65535 arasında olmalıdır");
+                    return;
+                }
+
+                System.Console.WriteLine(string.Format("IPAddress:{0} - Port {1}", ipaddr.ToString(), nPortInput));
+
+                client.Connect(ipaddr, nPortInput);
+
+
+
+                Console.WriteLine("Sunucuya bağlandı , Çıkmak için bir tuşa basınız");
+
+                string inputCommand = string.Empty;
+
+
+                //İstemci tarafından sunucuya hello! mesajı gönderilecek! 
+                inputCommand = "hello!";
+
+
+                byte[] buffSend = Encoding.ASCII.GetBytes(inputCommand);
+
+                client.Send(buffSend);
+
+                byte[] buffReceived = new byte[128];
+                int nRecv = client.Receive(buffReceived);
+
+                Console.WriteLine("Sunucu tarafından gelen mesaj: {0}", Encoding.ASCII.GetString(buffReceived, 0, nRecv));
+
+
+                Console.ReadKey();
+
+
+
+            }
+            catch (Exception excp)
+            {
+                Console.WriteLine(excp.ToString());
+            }
+
+            finally
+            {
+                if (client != null)
+                {
+                    if (client.Connected)
+                    {
+                        client.Shutdown(SocketShutdown.Both);
+                    }
+
+                    client.Close();
+                    client.Dispose();
+                }
+
+            }
+
+
+
+        }
+    }
+}
---
+++ server/server/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipaddr = IPAddress.Any;
+
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 34000);
+            try
+            {
+                listenerSocket.Bind(ipep);
+
+                listenerSocket.Listen(5);
+                
+                
+                Console.WriteLine("Sunucu bağlantı için hazır");
+
+                Socket client = listenerSocket.Accept();
+
+                Console.WriteLine("İstemci bağlandı" + " - Endpoint IP'si" + client.RemoteEndPoint.ToString());
+                byte[] buff = new byte[128];
+
+                int numberOfReceiveBytes = 0;
+
+                while (true)
+                {
+                    numberOfReceiveBytes = client.Receive(buff);
+
+
+                    string receivedText = Encoding.ASCII.GetString(buff, 0, numberOfReceiveBytes);
+
+                    string date = DateTime.Now.ToShortTimeString();
+
+                    byte[] buffSend = Encoding.ASCII.GetBytes(date);
+
+
+                    byte[] bufflast = buffSend.Concat(buff).ToArray();
+
+
+                    Console.WriteLine("İstemci tarafından gönderilen data: " + receivedText);
+                 
+                    client.Send(bufflast);
+
+
+                    Array.Clear(buff, 0, buff.Length);
+                    numberOfReceiveBytes = 0;
+                }
+
+            }
+            catch (Exception excp)
+            {
+                Console.WriteLine(excp.ToString());
+            }
+
+
+           
+        }
+    }
+}
